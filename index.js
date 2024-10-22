ff0eadde5930de7145779f7a89d7bbb6f4a41a5a
Shanu-Ojha <146368454+Shanu-Ojha@users.noreply.github.com>
2024-10-22 21:49:57 +0530

    File add from local

---
+++ index.js
@@ -0,0 +1,7 @@
+const bg1 = document.getElementById('bgColor-1');
+const bg2 = document.getElementById('bgColor-2');
+const bg3 = document.getElementById('bgColor-3');
+const blur = document.querySelector('.blur');
+document.querySelector('#bgChange').addEventListener('click', function() {
+    blur.style.background = `linear-gradient(90deg, ${bg1.value} 14%, ${bg2.value} 47%, ${bg3.value} 100%)`;
+});
